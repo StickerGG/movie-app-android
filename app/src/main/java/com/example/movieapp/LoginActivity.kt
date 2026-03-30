9038fb371fd7670c80cd0980ec1b6e32154f1323
Vadym <trickfire0@gmail.com>
2026-03-30 23:09:32 +0300

    Add LoginActivity

--- app/src/main/java/com/example/movieapp/LoginActivity.kt
+++ app/src/main/java/com/example/movieapp/LoginActivity.kt
@@ -1,20 +1,45 @@
 package com.example.movieapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.movieapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val login = binding.etLogin.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        val sharedPref = getSharedPreferences("USER_DATA", Context.MODE_PRIVATE)
+
+        val savedLogin = sharedPref.getString("login", "")
+        val savedPassword = sharedPref.getString("password", "")
+
+        if (login == savedLogin && password == savedPassword) {
+
+            // ✅ зберігаємо авторизацію
+            sharedPref.edit().putBoolean("isAuthorized", true).apply()
+
+            Toast.makeText(this, "Вхід успішний", Toast.LENGTH_SHORT).show()
+
+        } else {
+            Toast.makeText(this, "Невірний логін або пароль", Toast.LENGTH_SHORT).show()
         }
     }
 }
